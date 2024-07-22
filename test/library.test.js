341b3643f0fee4a8854120483d7c55e9d10b1083
Aviad Chen <50180656+aviadchen@users.noreply.github.com>
2024-07-22 17:55:59 +0300

    Update library.test.js

--- test/library.test.js
+++ test/library.test.js
@@ -1,14 +1,22 @@
 // test/library.test.js
 const { expect } = require('chai');
-const sinon = require('sinon');
 const filterAndExtractBookInfo = require('../library');
 
 describe('filterAndExtractBookInfo', () => {
   const books = [
-    { title: 'Book A', author: 'Author 1', genre: 'Fiction', year: 2001, isbn: '1111' },
-    { title: 'Book B', author: 'Author 2', genre: 'Fiction', year: 2005, isbn: '2222' },
-    { title: 'Book C', author: 'Author 3', genre: 'Non-Fiction', year: 2010, isbn: '3333' },
-    { title: 'Book D', author: 'Author 4', genre: 'Fiction', year: 1999, isbn: '4444' },
-    { title: 'Book E', author: 'Author 5', genre: 'Non-Fiction', year: 2003, isbn: '5555' }
+    { title: 'New Book', author: 'Author 1', genre: 'Fiction', year: 2010, isbn: '1' },
+    { title: 'Old Book', author: 'Author 2', genre: 'Fiction', year: 1999, isbn: '2' },
   ];
+
+  it('should filter books by genre and year', () => {
+    const genre = 'Fiction';
+    const minYear = 2000;
+    const properties = ['title'];
+
+    const result = filterAndExtractBookInfo(books, genre, minYear, properties);
+
+    expect(result.extractedInfo).to.deep.equal([
+      { title: 'New Book' },
+    ]);
+  });
 });
